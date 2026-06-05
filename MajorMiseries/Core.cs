97f295c417473054b727b9e132e62e4cbff6f6d4
EtherSystem <dermeisternew@gmail.com>
2026-06-05 15:28:05 +0200

    fix versionning

--- MajorMiseries/Core.cs
+++ MajorMiseries/Core.cs
@@ -2,7 +2,7 @@
 using MajorMiseries.Managers;
 using MajorMiseries.Persistence;
 
-[assembly: MelonInfo(typeof(MajorMiseries.Core), "Major Miseries", "1.1.0", "EtherSystem, FlowerField", null)]
+[assembly: MelonInfo(typeof(MajorMiseries.Core), "MajorMiseries", "1.1.1", "EtherSystem, FlowerField", null)]
 [assembly: MelonGame("Hinterland", "TheLongDark")]
 
 namespace MajorMiseries
